ef8e1ee2e162c9c667597842d60d9862c1c586b8
Nikolas Dahn <nikolas.dahn@dfki.de>
2022-08-18 17:41:05 +0200

    * Rearranged GUI a little bit * Switched to ttk variants of some widgets (Scale is unfortunately very incomplete) * Adjusted default window size

--- src/emg_robot/robot/direct_gui.py
+++ src/emg_robot/robot/direct_gui.py
@@ -9,7 +9,7 @@ def create_weight_row(tab, row, name, w, th):
     w_var = tk.DoubleVar(value=w)
     th_var = tk.DoubleVar(value=th)
 
-    label = tk.Label(tab, text=name)
+    label = ttk.Label(tab, text=name)
     label.grid(column=0, row=row, sticky="nw")
 
     w_slider = tk.Scale(tab, variable=w_var, from_=-1., to=1., resolution=0.05, orient=tk.HORIZONTAL)
@@ -29,6 +29,7 @@ class DirectControllerGUI(tk.Tk):
                  controller,
                  channel_names):
         super().__init__()
+        ttk.Style(self).theme_use('default')
 
         self.controller = controller
         self.control_thread = None
@@ -43,23 +44,8 @@ class DirectControllerGUI(tk.Tk):
         self.roll_f = tk.DoubleVar(value = controller.roll_f)
 
         self.title("Control Weights")
-        self.geometry("280x500")
+        self.geometry("640x350")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
-
-        # Some general options
-        tk.Label(self, text="Pitch factor").grid(column=0, row=0, sticky="nw")
-        pf_spin = ttk.Spinbox(self, textvariable=self.pitch_f, from_=0., to=10., increment=0.05)
-        pf_spin.grid(column=1, row=0, sticky="ne")
-        tk.Label(self, text="Roll factor").grid(column=0, row=1, sticky="nw")
-        rf_spin = ttk.Spinbox(self, textvariable=self.roll_f, from_=0., to=10., increment=0.05)
-        rf_spin.grid(column=1, row=1, sticky="ne")
-
-        self.progress = ttk.Progressbar(self, mode="indeterminate", orient=tk.HORIZONTAL)
-        self.progress.grid(column=0, row=2, sticky="ew")
-        self.grid_columnconfigure(0, weight=3)
-        self.btn_start_stop = tk.Button(self, text="Start", command=self.on_start_stop)
-        self.btn_start_stop.grid(column=1, row=2, sticky="ew")
-        ttk.Separator(self).grid(column=0, row=3, columnspan=2, sticky="ew")
 
         # Gains for transforming the emg signals
         tabs = ttk.Notebook(self)
@@ -67,14 +53,30 @@ class DirectControllerGUI(tk.Tk):
         tab_roll = ttk.Frame(tabs)
         tabs.add(tab_pitch, text="Pitch")
         tabs.add(tab_roll, text="Roll")
-        tabs.grid(column=0, row=4, columnspan=2, sticky="nsew")
+        tabs.grid(column=0, row=0, columnspan=2, sticky="nsew", pady=(5, 10))
+
+        # Some general options
+        ttk.Label(self, text="Pitch factor").grid(column=0, row=2, sticky="nw")
+        pf_spin = ttk.Spinbox(self, textvariable=self.pitch_f, from_=0., to=10., increment=0.05)
+        pf_spin.grid(column=1, row=2, sticky="ne")
+        ttk.Label(self, text="Roll factor").grid(column=0, row=3, sticky="nw")
+        rf_spin = ttk.Spinbox(self, textvariable=self.roll_f, from_=0., to=10., increment=0.05)
+        rf_spin.grid(column=1, row=3, sticky="ne")
+
+        self.btn_start_stop = ttk.Button(self, text="Start", command=self.on_start_stop)
+        self.btn_start_stop.grid(column=1, row=4, sticky="ew", pady=(7, 7))
+        self.progress = ttk.Progressbar(self, mode="indeterminate", orient=tk.HORIZONTAL)
+        self.progress.grid(column=0, row=5, columnspan=2, sticky="sew")
+        self.grid_columnconfigure(0, weight=3)
+        self.grid_rowconfigure(5, weight=1)
+
         
         # Create column labels for each tab
         for t in (tab_pitch, tab_roll):
             t.grid_columnconfigure(1, weight=3)
-            tk.Label(t, text="Muscle").grid(column=0, row=0, sticky="ew")
-            tk.Label(t, text="Weight").grid(column=1, row=0, columnspan=2, sticky="ew")
-            tk.Label(t, text="Threshold").grid(column=3, row=0, sticky="ew")
+            ttk.Label(t, text="Muscle").grid(column=0, row=0, sticky="ew")
+            ttk.Label(t, text="Weight").grid(column=1, row=0, columnspan=2, sticky="ew")
+            ttk.Label(t, text="Threshold").grid(column=3, row=0, sticky="ew")
 
         # Just for convenience
         cpw = controller.pitch_weights
@@ -102,7 +104,7 @@ class DirectControllerGUI(tk.Tk):
 
     def on_closing(self):
         self.stop_controller()
-        self.quit()
+        self.destroy()
 
     def start_controller(self):
         self.running = True
